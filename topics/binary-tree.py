896d8452bffeee6bfecaa0ab3210a896f7001f5c
HipsterSantos <santoscampos269@gmail.com>
2024-04-21 10:45:48 -0400

    [update] depth first tree traversal

---
+++ topics/binary-tree.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self,value,left=None,right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+a = TreeNode('A')
+b = TreeNode('B')
+c = TreeNode('C')
+d = TreeNode('D')
+e = TreeNode('E')
+f = TreeNode('F')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+
+def depthFirstSearch(root):
+    if root is None: return []
+    stack = [root]
+    result = []
+    while(len(stack)>0):
+        current_node = stack.pop()
+        print('pick current element , if have more add to traverse ',current_node.value)
+        result.append(current_node.value)
+        if current_node.right: stack.append(current_node.right)
+        if current_node.left: stack.append(current_node.left)
+
+
+depthFirstSearch(a)
+
+def recDepthFirst(root):
+    if root is None: return []
+    lefts = recDepthFirst(root.left)
+    rights = recDepthFirst(root.right)
+    return [root.value, *lefts, *rights ]
+
+print('result = ',recDepthFirst(a))
+
+print('pop--',[3,2,3,4].pop(1))
--- topics/linked-lists.py
+++ topics/linked-lists.py
@@ -8,6 +8,11 @@ b = Node('B')
 c = Node('C')
 d = Node('D')
 
+# initializing linked list
+a.next=b
+b.next=c
+c.next=d
+
 def printNodes(head):
     current = head
     while head is not None: 
@@ -27,3 +32,50 @@ def findNode(head,index,value=None):
         counter += 1
         head = head.next
 def reverseList(head):
+    current = head
+    prev = None
+    while current != None:
+        next = current.next
+        current.next = prev
+        prev = current
+        print('prev--',prev.value)
+        current = next
+    return prev
+
+def recReverseList(head,prev=None):
+    if head == None: return
+    print('item - ',prev,end='')
+    next = head.next
+    head.next = prev
+    return recReverseList(next,head)
+
+# reverse = recReverseList(a)
+# printNodes(reverse) /
+# aa = recReverseList(a)
+print('linked-list = ',a.value)
+
+def maxNode(head,counter=0):
+    if head == None:return counter
+    return maxNode(head.next,counter+1)
+print('total-nodes = ',maxNode(a))
+def zipLists(head1,head2):
+    tail = None
+    count = 0
+    while head1 != None and head2 != None:
+        if count % 2 == 0 :
+            tail = head1
+        else:
+            tail.next = head2
+    #find who have more nodes
+    # start1 = max(maxNode(head1),maxNode(head2))
+    #if head1 is greater than head 2 , loop should use head1 other than it should use head2
+    #on the iteraction make this replacement
+    """
+    current = head
+    next = current.next
+    toAdd = head2
+    current.next = toAdd
+    nextToAdd = toAdd.next
+    toAdd.next = next
+    toAdd = nextToAdd
+    """
