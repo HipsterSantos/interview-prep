421dbf5a1184f8c3bcb9cdfc1439314824dba17d
HipsterSantos <santoscampos269@gmail.com>
2024-04-22 04:41:32 -0400

    update: conver decimal to binary

---
+++ topics/binary-division.py
@@ -0,0 +1,18 @@
+#conver decimal to binary
+#example 10 ->
+
+def decimalToBinary(num):
+
+    binaryString = ""
+
+    def binaryDivisionHelper(num):
+        nonlocal binaryString
+        if num < 2:
+            binaryString += str(num)
+        else:
+            binaryDivisionHelper(num//2)
+            binaryDivisionHelper(num % 2)
+    binaryDivisionHelper(num)
+    return binaryString
+
+print(decimalToBinary(10))
