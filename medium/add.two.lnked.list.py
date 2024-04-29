88649240e4038031216b6f6de7131f1f29e476c5
HipsterSantos <santoscampos269@gmail.com>
2024-04-29 13:46:20 +0100

    update: update linkedlist challenges

--- medium/add.two.lnked.list.py
+++ medium/add.two.lnked.list.py
@@ -26,4 +26,50 @@ def sumTwoLists(l1,l2):
         if l2 is not None: l2 = l2.next
     return dumbList.next
         
-        
+        # class LinkedList(Node): 
+#     head = None
+#     size = 0
+#     def __init__(self, head: Node = None):
+#         if head != None:
+#             self.head = head
+#             self.size +=1
+#     def insert(self,node):
+#         if self.head is None: 
+#             self.head = node
+#         else:
+#             temp = self.head 
+#             self.head = node
+#             self.head.next = temp
+#         self.size +=1
+
+#     def insert(self,value,index):
+#         counter = 0
+#         while counter <=index:
+#             if counter == index: 
+
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         stack1,stack2 = [],[]
+#         while l1 != None and l2 != None:
+#             stack1.append(l1.val)
+#             stack2.append(l2.val)
+#             l1 = l1.next 
+#             l2 = l2.next
+#         #join all element of array 
+#         #342
+#         #465
+#         stack1str = [str(c) for c in stack1]
+#         stack2str = [str(b) for b in stack2]
+#         delimiter = ""
+#         str1 = int (delimiter.join(stack1str[::-1]))
+#         str2 = int(delimiter.join(stack2str[::-1]))
+
+#         print(f"stack1 {stack1str}  - stack2 {stack2str}")
+#         print(f"stack1 {str1}  - stack2 {str2}")
+
+#         summ = str1 + str2
+#         reminder = summ % 10
+#         newNode = ListNode(reminder)
+#         while summ /10 != 0:
+#             newNode.next(ListNode(reminder%10))
+#         return newNode
