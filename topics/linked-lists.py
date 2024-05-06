c93d26cd6b32cb5853ba520bef068a27ad9c76fa
HipsterSantos <santoscampos269@gmail.com>
2024-05-06 06:34:46 +0100

    solved: Linkedlist find the nth-node in the list

--- topics/linked-lists.py
+++ topics/linked-lists.py
@@ -131,3 +131,23 @@ class LinkedList:
     @property
     def totalNodes(self):
         return self.size
+    
+    
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val = val
+        self.next = next
+        
+def removeNthNode(head,n):
+    dummy = ListNode(0,head)
+    first,second = dummy,dummy
+    #once we create a dummy , now dummy have the following [0]->[1]->[2]->[3]->[4]->5] 
+    # original list = [1]->[2]->[3]->[4]->5]
+    for _ in range(n+1):
+        first = first.next # starts a 0 node till n+1 = n = 2 => 2+1 = 3 node [2]
+    while first is not None:
+        first = first.next
+        second = second.next
+    #next twice to skip the current element
+    second.next = second.next.next
+    return dummy.next
